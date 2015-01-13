71c32a14fbc39768d2b6c4cb3f41a9b52c165db0
Chunyang Xu <xuchunyang56@gmail.com>
2015-01-13 17:51:23 +0800

    Basic config for trans between Chinese and English

---
+++ google-translate-config.el
@@ -0,0 +1,65 @@
+(require 'popup)
+(require 'chinese-word-at-point)
+(require 'google-translate)
+
+(setq google-translate-base-url
+      "http://translate.google.cn/translate_a/t")
+
+(setq google-translate-listen-url
+      "http://translate.google.cn/translate_tts")
+
+(defun google-translate-chinese--region-or-word ()
+  (if (use-region-p)
+      (buffer-substring-no-properties (region-beginning)
+                                      (region-end))
+    (thing-at-point 'chinese-or-other-word t)))
+
+(defun google-translate-chinese--translate (word)
+  (google-translate-json-translation
+   (if (chinese-word-cjk-string-p word)
+       (google-translate-request "zh-CN" "en" word)
+     (google-translate-request "en" "zh-CN" word))))
+
+(defun google-translate-chinese--detailed-translate (word)
+  (google-translate-json-detailed-translation
+   (if (chinese-word-cjk-string-p word)
+       (google-translate-request "zh-CN" "en" word)
+     (google-translate-request "en" "zh-CN" word))))
+
+(defun google-translate-chinese-at-point ()
+  "Translate at point and show full result with buffer."
+  (interactive)
+  (let ((word (google-translate-chinese--region-or-word)))
+    (if word
+        (if (chinese-word-cjk-string-p word)
+            (google-translate-translate "zh-CN" "en" word)
+          (google-translate-translate "en" "zh-CN" word))
+      (message "Nothing to translate"))))
+
+(defun google-translate-chinese-at-point-echo-area ()
+  "Translate at point and show only translation in echo area. "
+  (interactive)
+  (let ((word (google-translate-chinese--region-or-word)))
+    (if word
+        (message (google-translate-chinese--translate word))
+      (message "Nothing to translate"))))
+
+(defun google-translate-chinese-query ()
+  "Translate input and show full result with buffer."
+  (interactive)
+  (let* ((word (google-translate-chinese--region-or-word))
+         (word (read-string (format "Translate (%s): "
+                                    (or word ""))
+                            nil nil
+                            word)))
+    (if word
+        (if (chinese-word-cjk-string-p word)
+            (google-translate-translate "zh-CN" "en" word)
+          (google-translate-translate "en" "zh-CN" word))
+      (message "Nothing to translate"))))
+
+(defun google-translate-chinese--detailed-translate (word)
+  (google-translate-json-detailed-translation
+   (if (chinese-word-cjk-string-p word)
+       (google-translate-request "zh-CN" "en" word)
+     (google-translate-request "en" "zh-CN" word))))
