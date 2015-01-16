bd96bb8cb330026c5ee96433a731a7d4a02e029f
Chunyang Xu <xuchunyang56@gmail.com>
2015-01-16 10:31:35 +0800

    Better way to replace old word

--- google-translate-config.el
+++ google-translate-config.el
@@ -19,6 +19,11 @@
                                       (region-end))
     (thing-at-point 'chinese-or-other-word t)))
 
+(defun -bounds-region-or-word ()
+  (if (use-region-p)
+      (cons (region-beginning) (region-end))
+    (bounds-of-thing-at-point 'chinese-or-other-word)))
+
 (defun -translate (word)
   (google-translate-json-translation
    (if (chinese-word-cjk-string-p word)
@@ -74,8 +79,9 @@
 (defun search-at-point-and-replace ()
   "可以不写 DocString."
   (interactive)
-  (let ((word (-region-or-word)))
-    (if word
+  (let ((word (-region-or-word))
+        (bounds (-bounds-region-or-word)))
+    (if (and word bounds)
         (let (explains
               popup-list
               (detailed-translate (-detailed-translate word))
@@ -84,7 +90,7 @@
               (progn
                 (setq selected-item (popup-menu* (list (popup-make-item (-translate word)))))
                 ;; 此处假设了光标在词的后面，而不是其它位置，下同
-                (delete-char (- (length word)))
+                (delete-region (car bounds) (cdr bounds))
                 (insert selected-item))
             (loop for item across detailed-translate do
                   (let ((index 0))
@@ -104,7 +110,7 @@
                       a-popup-menu)
                 (setq index (+ index 2)))
               (setq selected-item (popup-menu* (reverse a-popup-menu)))
-              (delete-char (- 0 (length word)))
+              (delete-region (car bounds) (cdr bounds))
               (insert (substring selected-item 3)))))
       (message "Nothing to translate"))))
 
